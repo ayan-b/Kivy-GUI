53ed120c74a192ace479de8bf41c43037ad6769f
ayan-b <ayanbanerjee7777@gmail.com>
2019-09-12 17:26:20 +0530

    Add background

--- Controller.py
+++ Controller.py
@@ -1,24 +1,40 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, CardTransition
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.core.window import Window
+Window.fullscreen = True
 
 import add
 
 class MainScreen(Screen):
-    pass
+    def open_popup(self, reading):
+        the_popup = Popup(title='Reading', content = Label(text=str(reading)), 
+                      size_hint=(0.5, 0.5), size=(200, 200))
+        the_popup.open()
 
 class ScreenTwo(Screen):
-    pass
+    def open_popup(self, reading):
+        the_popup = Popup(title='Reading', content = Label(text=str(reading)), 
+                      size_hint=(0.5, 0.5), size=(200, 200))
+        the_popup.open()
 
 class ScreenManagement(ScreenManager):
     pass
 
-presentation = Builder.load_file("Controller.kv")
 
-class Controller(App):
+Builder.load_file("Controller.kv")
+presentation = ScreenManager()
+presentation.add_widget(MainScreen())
+presentation.add_widget(ScreenTwo())
+
+
+class ControllerApp(App):
     def build(self):
         return presentation
 
 
 if __name__ == '__main__':
-    Controller().run()
+    ControllerApp().run()
--- add.py
+++ add.py
@@ -1,2 +1,2 @@
-def add(a,b):
-    print (a+b)
+def addd(a,b):
+    return a+b
